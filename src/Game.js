2705c4201773f063a7d8224c905e811b082f84e1
Adrian Wong <adrianwjw@gmail.com>
2017-07-15 11:55:39 +1000

    Add HUD elements

--- src/Game.js
+++ src/Game.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import './Game.css';
 
 class Game extends Component {
+  constructor() {
+    super();
+    this.state = {
+      playerLives : 3,
+      playerScore : 0
+    }
+  }
+
   render() {
     return (
       <div className="Game">
         <div className="Background" />
         <div className="Ground" />
+        <div className="Hud">
+          <span className="PlayerName">Mario</span>
+          <span className="PlayerLives">{this.state.playerLives}</span>
+          <div className="ItemContainer" />
+          <span className="PlayerScore">{this.state.playerScore}</span>
+        </div>
       </div>
     );
   }
